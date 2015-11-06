501b4ce288d1f12e2deb082f4956edf04b42e233
Andrey Raykov <raykov.andrey@gmail.com>
2015-11-06 21:51:03 +0200

    let's try other auth type

--- config/initializers/_load_config.rb
+++ config/initializers/_load_config.rb
@@ -31,7 +31,7 @@ unless defined?(Errbit::Config)
     Errbit::Config.smtp_settings = {
       :address        => ENV['SMTP_SERVER'] || 'smtp.sendgrid.net',
       :port           => ENV['SMTP_PORT']   || 25,
-      :authentication => :plain,
+      :authentication => :login,
       :user_name      => ENV['SMTP_USERNAME']   || ENV['SENDGRID_USERNAME'],
       :password       => ENV['SMTP_PASSWORD']   || ENV['SENDGRID_PASSWORD'],
       :domain         => ENV['SMTP_DOMAIN'] || ENV['SENDGRID_DOMAIN'] || ENV['ERRBIT_EMAIL_FROM'].split('@').last
